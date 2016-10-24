be4de32954f220c0b4ef53f529e70862fa0d5c65
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-10-24 16:25:32 -0400

    Creating the removeColumns function to apply the nearPD.
    Also removing old commented code.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -117,114 +117,23 @@ void nearPositiveDefinite(Eigen::MatrixXd &mat,double noise){
   mat = es.eigenvectors().real()*rLambda*es.eigenvectors().real().inverse();
 }
 
-
-/*
-void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove)
-{
-  unsigned int numRows = matrix.rows()-1;
-  unsigned int numCols = matrix.cols();
-
-  if( rowToRemove < numRows )
-    matrix.block(rowToRemove,0,numRows-rowToRemove,numCols) = matrix.block(rowToRemove+1,0,numRows-rowToRemove,numCols);
-
-  matrix.conservativeResize(numRows,numCols);
-}
-
-void removeColumn(Eigen::MatrixXd& matrix, unsigned int colToRemove)
-{
-  unsigned int numRows = matrix.rows();
-  unsigned int numCols = matrix.cols()-1;
-
-  if( colToRemove < numCols )
-    matrix.block(0,colToRemove,numRows,numCols-colToRemove) = matrix.block(0,colToRemove+1,numRows,numCols-colToRemove);
-
-  matrix.conservativeResize(numRows,numCols);
-}
-
-
-Eigen::VectorXi compareGreat(Eigen::VectorXd d,double e1){
-  Eigen::VectorXi v(d.size());
-  for(int i=0;i<d.size();i++){
-    if(d(i)>e1){
-      v(i)=1; //True
-    }
-    else{
-      v(i)=0; //False
-    }
-  }
-  return(v);
-}
-
-Eigen::MatrixXd removeColumns(Eigen::MatrixXd matrix, Eigen::VectorXi colToRemove)
-{
-  int col = colToRemove.sum();
-  int cont = 0;
-  Eigen::MatrixXd matrix0(matrix.rows(),col);
-  for(int c=0;c<matrix.cols();c++){
-    if(colToRemove(c)==1){
-      for(int r=0;r<matrix.rows();r++){
-            matrix0(r,cont)=matrix(r,c);
+//Remove columns
+Eigen::MatrixXd removeColumns(Eigen::MatrixXd Q, Eigen::VectorXd d, double e1){
+  //Find the number of columns
+  int ncols = (d.array()>e1).count();
+  //Create the new matrix
+  Eigen::MatrixXd Q0(Q.rows(),ncols);
+  //Initialize the counter
+  int cont=0;
+  for(int c=0;c<Q.cols();c++){
+    if(d(c)>e1){
+      for(int r=0;r<Q.rows();r++){
+        Q0(r,cont) = Q(r,c);
       }
-      cont=cont+1;
+      cont = cont+1;
     }
   }
-  return(matrix0);
+  return(Q0);
 }
 
-void nearPDefinite(Eigen::MatrixXd &mat, int maxit, double eigtol = 1e-06){
-  int n = mat.cols();
-  Eigen::MatrixXd D_S = Eigen::MatrixXd::Zero(mat.rows(),mat.cols());
-  Eigen::MatrixXd X =mat;
-  int iter = 0;
-  bool converged = false;
-  double conv = std::numeric_limits<double>::infinity();
-  while (iter < maxit && !converged) {
-    Eigen::MatrixXd Y = X;
-    Eigen::MatrixXd R = Y - D_S;
-    //Getting the eigenvalues
-    Eigen::EigenSolver<Eigen::MatrixXd> e(R,true);
-    Eigen::MatrixXd Q = e.eigenvectors().real();
-    Eigen::VectorXd d = e.eigenvalues();
-    double e1 = eigtol*d(1);
-    Eigen::VectorXi p = compareGreat(d,e1);
-    bool test = (p.array() > 0).any();
-    if (!test){
-      stop("Matrix seems negative semi-definite")
-    }
-    Eigen::MatrixXd Q0 = removeColumns(Q,p);
-    Eigen::VectorXd d0 = removeElements(d,p);
-    Eigen::MatrixXd Qtemp
-    X <- tcrossprod(Q0 * rep(d[p], each = nrow(Q0)), Q0)
-      if (doDykstra)
-        D_S <- X - R
-        if (doSym)
-          X <- (X + t(X))/2
-        if (corr)
-          diag(X) <- 1
-        else if (keepDiag)
-          diag(X) <- diagX0
-        conv <- norm(Y - X, conv.norm.type)/norm(Y, conv.norm.type)
-        iter <- iter + 1
-        if (trace)
-          cat(sprintf("iter %3d : #{p}=%d, ||Y-X|| / ||Y||= %11g\n",
-                      iter, sum(p), conv))
-        converged <- (conv <= conv.tol)
-  }
-  if (do2eigen || only.values) {
-    e <- eigen(X, symmetric = TRUE)
-    d <- e$values
-    Eps <- posd.tol * abs(d[1])
-    if (d[n] < Eps) {
-      d[d < Eps] <- Eps
-      if (!only.values) {
-        Q <- e$vectors
-        o.diag <- diag(X)
-        X <- Q %*% (d * t(Q))
-        D <- sqrt(pmax(Eps, o.diag)/diag(X))
-        X[] <- D * X * rep(D, each = n)
-      }
-    }
-  }
-  return(X);
-}
-*/
+
