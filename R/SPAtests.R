f13f787f324a0ee63ba3efd70d1665cf723851d5
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-11-04 11:24:33 -0400

    Add White's Reality Check.

--- R/SPAtests.R
+++ R/SPAtests.R
@@ -70,4 +70,61 @@ hansen.spa <- function(Dmat,bVec,typeFunc,B,geomMean,bandwidth) {
 }
 
 
+#' Execute the White, H. (2000) Reality Check Test
+#'
+#' @param Dmat A matrix with the relative performance variables (NxK)
+#' @param bVec A vector with the relative performance benchmark (Nx1)
+#' @param typeFunc Type of function to be used:
+#' 0 - max(0,x)
+#' 1 - x*1_{x >= -sqrt{(w^2/n)2log log n}}
+#' 2 - x
+#' @param B total of boostraps samples.
+#' @param geomMean mean of the geometric distribution used to generate the block lengths
+#' @return Statistic of the test and p-value
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+white.spa <- function(Dmat,bVec,typeFunc,B,geomMean,bandwidth) {
+  #Step 0: Computes the performance of model k relative to the benchmark at time t.
+  d.mat <- apply(Dmat,2,function(x)x-bVec)
+
+  #Size of the Time series
+  n<-length(bVec)
+
+  #Step 1: Compute d.bar
+  d.bar  <- colMeans(d.mat)
+
+  ##White works with a non standardized statistic
+  omega <-rep(1,ncol(Dmat))
+
+  # Computes the T.spa Statistic
+  t.SPA <- max(max(sqrt(n)*d.bar/omega),0)
+
+  #Apply the Hansen function
+  if(typeFunc==0){
+    gFunc <- apply(as.matrix(d.bar),1,function(x) max(x,0))
+  }else if(typeFunc==1){
+    gFunc <- apply(as.matrix(d.bar),1,function(x) x*ifelse(x>=-sqrt(((omega^2)/n)*log(log(n))),1,0))
+  }else{
+    gFunc <- d.bar
+  }
+  Z <- t(apply(d.mat,1,function(x) x - gFunc))
+
+  #Stationary Bootstrap
+  Zboot <- boot::tsboot(Z,statistic=colMeans, R=B,l=geomMean,sim="geom")$t*sqrt(n)
+
+  #Boostrap T.SPA
+  T.SPA <- t(apply(Zboot,1,function(x) x/omega))
+
+  #For each time series:
+  T.SPA <- apply(as.matrix(apply(T.SPA,1,max)),1,function(x) max(x,0))
+
+  #P-value
+  p.value <- mean(T.SPA>t.SPA)
+
+  #Return function
+  return(list("White's RC statistic"=t.SPA,
+              "P-value"=p.value))
+}
+
 
