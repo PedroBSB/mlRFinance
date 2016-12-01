28ae3a2de424cd1eeed9411a39717c02c7a70a1f
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-12-01 15:49:37 -0500

    Create condition

--- R/QPFS.R
+++ R/QPFS.R
@@ -1,9 +1,11 @@
 #Rodriguez-Lujan, Irene, et al. "Quadratic programming feature selection."
 #Journal of Machine Learning Research 11.Apr (2010): 1491-1516.
-QPFS<-function(y,X,alpha){
+QPFS<-function(y,X,alpha,type){
   #Compute the Similarity Matrix
-  Q <- cor(X)
-  Q <- abs(Q)
+  if(type="Pearson"){
+    Q <- cor(X)
+    Q <- abs(Q)
+  }
   #Compute the Relevance (binary problem)
   prob <- mean(y==+1)
   f <- diag(Q)*prob
@@ -13,3 +15,7 @@ QPFS<-function(y,X,alpha){
   return(weights)
 }
 
+# X<-cbind(rnorm(1000),rnorm(1000),rnorm(1000),rnorm(1000))
+# y<-rbinom(1000,1,0.2)
+# y[y==0]<- -1
+# alpha<-NA
