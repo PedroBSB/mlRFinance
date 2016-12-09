530ec2b17adc4ae637b8362411f1d715f1752b58
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-12-09 11:01:58 -0500

    Update dataset and functions

--- tests/SpatialWOCSVM.R
+++ tests/SpatialWOCSVM.R
@@ -13,7 +13,7 @@ nb <- spdep::poly2nb(shp)
 wMat <- spdep::nb2mat(nb, style="W", zero.policy =TRUE)
 
 #Data matrix
-A<-as.matrix(scale(Crime[,c(2:4)]))
+A<-as.matrix(scale(Crime[,c(2:8)]))
 
 #Run the SpatialWOCSCM
 svc<-SpatialWOCSCM(A, wMat, 0.1, 2, 0.01, 1, 100, "Gaussian", c(0.5))
@@ -38,5 +38,8 @@ sp::spplot(shp2, "Cluster", col.regions=col, main="Clusters",
        colorkey = FALSE, lwd=.4, col="white")
 
 
+#Get the spatial matrix for each cluster
+shp3 <- split(shp2, shp2$Cluster)
+nb <-lapply(shp3,function(x) spdep::nb2mat(spdep::poly2nb(x), style="B", zero.policy =TRUE) )
 
 
