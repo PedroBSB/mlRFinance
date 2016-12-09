112379e2bea96977f89016a71d28d958fc5da689
PedroBSB <pedro.melo.albuquerque@gmail.com>
2016-12-09 10:41:40 -0500

    New model

--- tests/SpatialWOCSVM.R
+++ tests/SpatialWOCSVM.R
@@ -16,7 +16,7 @@ wMat <- spdep::nb2mat(nb, style="W", zero.policy =TRUE)
 A<-as.matrix(scale(Crime[,c(2:4)]))
 
 #Run the SpatialWOCSCM
-svc<-SpatialWOCSCM(A, wMat, 0.5, 2, 1, 1, 100, "Gaussian", c(0.5))
+svc<-SpatialWOCSCM(A, wMat, 0.1, 2, 0.01, 1, 100, "Gaussian", c(0.5))
 
 #Plot the log-likelihood
 plot(svc$LogLikelihood, type="l")
@@ -39,38 +39,4 @@ sp::spplot(shp2, "Cluster", col.regions=col, main="Clusters",
 
 
 
-
-
-
-
-
-
-#Pequeno exemplo
-A<-matrix(c(1,2,5,6,
-            5,5,2,1,
-            8,1,1,7),nrow=4,ncol=3)
-
-wMat<-matrix(c(0,1,0,5,
-               1,0,0,0,
-               0,0,0,2,
-               5,0,2,0),byrow = TRUE, nrow=4, ncol=4)
-
-
-svc<-SpatialWOCSCM(A,wMat, 1, 2, 1, 100, "Gaussian", c(0.5))
-
-svc
-
-#Pequeno grande
-data(iris)
-df<-as.matrix(iris[,1:4])
-
-
-#WOCSCM(X, C, k, sigma, inter, kernel, parms)
-svc<-WOCSCM(df, 0.05, 2, 1.2, 100, "Gaussian", c(0.5))
-svc
-
-head(iris)
-teste<-cbind(iris$Species,svc$Zmat)
-teste[,2:3]<-round(teste[,2:3])
-table(teste[,1],teste[,2])
 
