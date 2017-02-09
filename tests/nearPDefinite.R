5f31f5b2336387fde0eb412bbfb00696b31e9f95
PedroBSB <pedro.melo.albuquerque@gmail.com>
2017-02-09 11:04:44 -0500

    Complement the nearPD algorithm using eigenvalues to correct some deviations.

--- tests/nearPDefinite.R
+++ tests/nearPDefinite.R
@@ -23,4 +23,51 @@ nearPDefinite(K)
 
 Matrix::nearPD(K)
 
+#########################################
+
+#Load the data
+data("sinc")
+#Separate the data
+ids <-sample(1:nrow(sinc),30)
+train <- as.matrix(sinc[ids,])
+#Order the dataset
+train <- train[order(train[,1]),]
+#Validation
+valid <- as.matrix(sinc[-ids,])
+#Order the dataset
+valid <- valid[order(valid[,1]),]
+#Parameters
+n <- nrow(train)
+C <- 1.0
+epsilon <- 0.5
+d <- train[,2]
+A <- as.matrix(train[,1])
+#### Solve by hand
+K <- mlRFinance::KernelMatrixComputation(A,"Gaussian",0.5)
+# Q matrix
+Q <- rbind(cbind(K,-K),cbind(-K,+K))
+
+##
+Qnear <- Matrix::nearPD(Q, corr = FALSE, keepDiag = TRUE, do2eigen = TRUE,
+                        doSym = FALSE, doDykstra = FALSE, only.values = FALSE,
+                        ensureSymmetry = FALSE,
+                        eig.tol = 1e-06, conv.tol = 1e-07, posd.tol = 1e-08,
+                        maxit = 100, conv.norm.type = "I", trace = FALSE)
+Qnear <- Qnear$mat
+
+
+#############################################
+n <- 3
+vecD <- c(-1,-2,-3)
+X <-matrix(rnorm(9,100),ncol=3,nrow=3)
+
+vecD*X*rep(vecD, each = n)
+
+
+
+for(r in 1:n){
+  for(c in 1:n){
+    X[r,c] = vecD[r]*X[r,c]*vecD[c];
+  }
+}
 
