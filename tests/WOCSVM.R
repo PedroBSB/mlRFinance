e238a6ab2902d5f4f357227368af9023b9c3b605
PedroBSB <pedro.melo.albuquerque@gmail.com>
2017-02-23 11:03:35 -0500

    Update system files

--- tests/WOCSVM.R
+++ tests/WOCSVM.R
@@ -15,10 +15,10 @@ df<-as.matrix(iris[,1:4])
 
 
 #WOCSCM(X, C, k, sigma, inter, kernel, parms)
-svc<-WOCSCM(df, 1, 2, 1.2, 100, "Gaussian", c(0.5))
+svc<-WOCSCM(df, 0.2, 2, 0.005, 100, "Gaussian", c(0.5))
 svc
 
 head(iris)
 teste<-cbind(iris$Species,svc$Zmat)
-teste[,2:3]<-round(teste[,2:3])
+teste[,2:4]<-round(teste[,2:4])
 table(teste[,1],teste[,2])
